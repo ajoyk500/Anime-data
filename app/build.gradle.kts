28090159a8f68d58fccd27e55decc74d13c6bab6
ajoyk500 <ajoykundu500@gmail.com>
2025-11-20 21:01:35 +0530

    update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -80,7 +80,7 @@ android {
 
     signingConfigs {
     create("release") {
-        storeFile = file("my-release-key.jks")
+        storeFile = file("../my-release-key.jks")
         storePassword = System.getenv("KEYSTORE_PASSWORD")
         keyAlias = System.getenv("KEY_ALIAS")
         keyPassword = System.getenv("KEY_PASSWORD")
