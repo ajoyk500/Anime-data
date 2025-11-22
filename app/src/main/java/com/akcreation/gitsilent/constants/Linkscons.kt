e78d86b9141508b4026b3990a12db99120dd9812
ajoyk500 <ajoykundu500@gmail.com>
2025-11-22 10:36:27 +0530

    update

---
+++ app/src/main/java/com/akcreation/gitsilent/constants/Linkscons.kt
@@ -0,0 +1,8 @@
+package com.akcreation.gitsilent.constants
+
+object StrCons {
+    const val reportBugsLink = "https://github.com/AKCREATION-IND/YourRepo/issues"
+    const val donateLink = "https://your-donate-page.com"
+    const val automationDocUrl = "https://your-docs-page.com/automation"
+    const val httpServiceApiUrl = "https://your-api-url.com"
+}
